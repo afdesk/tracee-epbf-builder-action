9f8cc6cf801398909a28d11e7560f4185ba493d2
AMF <work@afdesk.com>
2021-07-06 19:05:45 +0600

    Init commit

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+wget https://github.com/afdesk/my-driverkit/releases/download/v0.5.3/driverkit_linux_x86_64.tar.gz
+tar -xf driverkit_linux_x86_64.tar.gz
+./driverkit docker -c ubuntu.yaml --kernelrelease=$1 --output-probe=$2
+
